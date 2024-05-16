a9192739f198180714d4af93ca6cba73aa8b4238
Erick Patiño <erickfco1999@gmail.com>
2024-05-16 14:16:21 -0600

    Control form items's behavior

--- app/public/js/actions_forms.js
+++ app/public/js/actions_forms.js
@@ -2,7 +2,7 @@ const btnOpenContainer = document.querySelectorAll(".open-form-container");
 const formContainer = document.querySelectorAll(".form-container");
 const btnCloseContainer = document.querySelectorAll(".close-form-container");
 const form = document.querySelector("#form-tool");
-const formItem = document.querySelector(".form-item");
+const formItems = document.querySelectorAll(".form-item");
 const formSupplier = document.querySelector(".supplier-form");
 
 /**
@@ -56,35 +56,37 @@ form?.querySelectorAll(".required").forEach((element) => {
 })
 
 // ?? Search all elements inside form that have required class added
-formItem?.querySelectorAll(".required").forEach((element) => {
-    // ?? Select each label that contains required class
-    const label = element.parentElement;
-    // ?? Select each input 
-    const input = element.parentElement.parentElement.querySelector(".input-required");
-    // ?? Select each select
-    const select = element?.parentElement.parentElement.querySelector(".select-required");
-    // ?? Event listener to check if user is typing
-    input?.addEventListener("keyup", (e) => {
-        const value = e.target.value;
-        // ?? Check if input is empty
-        if(value.trim() != "") {
-            label.style.borderColor = "#000"
-        } else {
-            label.style.borderColor = "#f00"
-        }
-    })
-    // ?? Event listener to check if user selects an option
-    select?.addEventListener("change", (e) => {
-        // ?? Get the index from the select tag
-        const value = e.target.selectedIndex;
-        // ?? Get the text from the selected option
-        const text = e.target.options[value].text;
-        // ?? Check if option text is empty
-        if(text != "") {
-            label.style.borderColor = "#000"
-        } else {
-            label.style.borderColor = "#f00"
-        }
+formItems?.forEach(formItem => {
+    formItem?.querySelectorAll(".required").forEach((element) => {
+        // ?? Select each label that contains required class
+        const label = element.parentElement;
+        // ?? Select each input 
+        const input = element.parentElement.parentElement.querySelector(".input-required");
+        // ?? Select each select
+        const select = element?.parentElement.parentElement.querySelector(".select-required");
+        // ?? Event listener to check if user is typing
+        input?.addEventListener("keyup", (e) => {
+            const value = e.target.value;
+            // ?? Check if input is empty
+            if(value.trim() != "") {
+                label.style.borderColor = "#000"
+            } else {
+                label.style.borderColor = "#f00"
+            }
+        })
+        // ?? Event listener to check if user selects an option
+        select?.addEventListener("change", (e) => {
+            // ?? Get the index from the select tag
+            const value = e.target.selectedIndex;
+            // ?? Get the text from the selected option
+            const text = e.target.options[value].text;
+            // ?? Check if option text is empty
+            if(text != "") {
+                label.style.borderColor = "#000"
+            } else {
+                label.style.borderColor = "#f00"
+            }
+        })
     })
 })
 
